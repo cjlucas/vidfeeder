a69ae348ce667925e02fd5ef8024e4193efb480d
Chris Lucas <chris@chrisjlucas.com>
2018-10-27 17:45:28 -0400

    Remove brunch support (#3)

--- config/dev.exs
+++ config/dev.exs
@@ -10,9 +10,7 @@ config :vidfeeder, VidFeederWeb.Endpoint,
   http: [port: 5000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  check_origin: false
 
 # ## SSL Support
 #
