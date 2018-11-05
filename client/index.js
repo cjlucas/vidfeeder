e86a536c8314726a082033a4a0d76280dacdf048
Chris Lucas <chris@cjl.io>
2018-11-04 20:26:44 -0500

    Webpack

---
+++ client/index.js
@@ -0,0 +1,4 @@
+import { Elm } from './src/Main'
+import css from './styles.css'
+
+Elm.Main.init({});
---
+++ client/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'index.js'),
+  ],
+  resolve: {
+    extensions: ['.js', '.elm'],
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
