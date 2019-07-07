a37a74b9f51e148163ae0871282ecf03d9fa74fc
Chris Lucas <chris@chrisjlucas.com>
2019-07-07 19:00:10 -0400

    Add Vector to provision playbook

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -z "$ELASTICSEARCH_URL" ] && echo "Error: ELASTICSEARCH_URL is not set" && exit 1
+
 if [[ -z $1 ]]; then
     echo "Usage: $0 <host group>"
     exit 1
@@ -11,4 +13,6 @@ ansible-playbook .ansible/playbooks/provision.yml \
     --user root \
     --extra-vars="ansible_hosts=$1" \
     --extra-vars="ansible_ssh_common_args='-o StrictHostKeyChecking=no'" \
-    --extra-vars="ansible_python_interpreter=/usr/bin/python3"
+    --extra-vars="ansible_python_interpreter=/usr/bin/python3" \
+    --extra-vars="tag_name=$1" \
+    --extra-vars="elasticsearch_url=$ELASTICSEARCH_URL"
