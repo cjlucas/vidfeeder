52ac501d3d8aaa718180016228af8ae711bd2415
Chris Lucas <chris@chrisjlucas.com>
2019-07-13 14:32:19 -0400

    Add S3 sink

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -z "$AWS_ACCESS_KEY_ID" ] && echo "Error: AWS_ACCESS_KEY_ID is not set" && exit 1
+[ -z "$AWS_SECRET_KEY" ] && echo "Error: AWS_SECRET_KEY is not set" && exit 1
 [ -z "$ELASTICSEARCH_URL" ] && echo "Error: ELASTICSEARCH_URL is not set" && exit 1
 
 if [[ -z $1 ]]; then
@@ -15,4 +17,6 @@ ansible-playbook .ansible/playbooks/provision.yml \
     --extra-vars="ansible_ssh_common_args='-o StrictHostKeyChecking=no'" \
     --extra-vars="ansible_python_interpreter=/usr/bin/python3" \
     --extra-vars="tag_name=$1" \
+    --extra-vars="aws_access_key=$AWS_ACCESS_KEY_ID" \
+    --extra-vars="aws_secret_key=$AWS_SECRET_KEY" \
     --extra-vars="elasticsearch_url=$ELASTICSEARCH_URL"
