afe137cf4012292e6042b32f1259bdc88f9e71a7
Chris Lucas <chris@chrisjlucas.com>
2020-11-01 01:12:43 -0400

    Add support for multiple SourceProcessor workers

--- lib/vidfeeder/source_processor_supervisor.ex
+++ lib/vidfeeder/source_processor_supervisor.ex
@@ -2,10 +2,14 @@ defmodule VidFeeder.SourceProcessorSupervisor do
   use Supervisor
 
   def start_link(_opts) do
-    children = [
-      VidFeeder.SourceProcessor
-    ]
+    children = workers(3, VidFeeder.SourceProcessor, [])
 
     Supervisor.start_link(children, strategy: :one_for_one, name: __MODULE__)
+  end
+
+  defp workers(times, module, opts) do
+    Enum.map(1..times, fn i ->
+      Supervisor.child_spec({module, opts}, id: "#{module}_#{i}")
+    end)
   end
 end
