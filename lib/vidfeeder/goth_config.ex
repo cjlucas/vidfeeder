b756e78cc93956d3330d834a9beeec754f096274
Chris Lucas <chris.lucas@thelevelup.com>
2021-12-12 18:48:34 -0500

    remove cipher

--- lib/vidfeeder/goth_config.ex
+++ lib/vidfeeder/goth_config.ex
@@ -2,13 +2,10 @@ defmodule VidFeeder.GothConfig do
   use Goth.Config
 
   def init(config) do
-    {:ok, Keyword.put(config, :json, decrypt_credentials!())}
+    {:ok, Keyword.put(config, :json, credentials())}
   end
 
-  def decrypt_credentials! do
-    Application.app_dir(:vidfeeder, "priv/credentials")
-    |> Path.join("vidfeeder_service_account_credentials")
-    |> File.read!()
-    |> Cipher.decrypt()
+  def credentials do
+    System.fetch_env!("GOOGLE_APPLICATION_CREDENTIALS") |> Base.decode64!
   end
 end
